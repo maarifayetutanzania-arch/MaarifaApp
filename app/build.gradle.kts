e8f224cde9df066fbc33f3afb9be1d8976275364
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-08-17 22:06:36 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,6 +79,7 @@ android {
 
 dependencies {
     // Core / Compose
+implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.13.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
