0e907e61d140ad262f0c2697e7728732305fa4b4
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-08-18 14:49:28 +0300

    Update MaarifaApplication.kt

--- app/src/main/java/com/maarifa/app/MaarifaApplication.kt
+++ app/src/main/java/com/maarifa/app/MaarifaApplication.kt
@@ -1,6 +1,7 @@
 package com.maarifa.app
 
 import android.app.Application
+import com.google.firebase.FirebaseApp
 import com.maarifa.app.di.AppContainer
 
 class MaarifaApplication : Application() {
@@ -9,6 +10,11 @@ class MaarifaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
+        
+        // 1. Initialize Firebase kwanza kabisa
+        FirebaseApp.initializeApp(this)
+
+        // 2. Ndio utengeneze container
         container = AppContainer.get(this)
     }
 }
