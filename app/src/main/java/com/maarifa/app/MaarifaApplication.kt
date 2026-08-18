cf6fc2024c3e620ad737550e9edc5e0d80265007
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-08-18 15:26:08 +0300

    Update MaarifaApplication.kt

--- app/src/main/java/com/maarifa/app/MaarifaApplication.kt
+++ app/src/main/java/com/maarifa/app/MaarifaApplication.kt
@@ -2,6 +2,7 @@ package com.maarifa.app
 
 import android.app.Application
 import com.google.firebase.FirebaseApp
+import com.google.firebase.FirebaseOptions
 import com.maarifa.app.di.AppContainer
 
 class MaarifaApplication : Application() {
@@ -10,11 +11,20 @@ class MaarifaApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        
-        // 1. Initialize Firebase kwanza kabisa
-        FirebaseApp.initializeApp(this)
 
-        // 2. Ndio utengeneze container
+        // Initialize Firebase manual kwa kutumia credentials kutoka kwako
+        if (FirebaseApp.getApps(this).isEmpty()) {
+            val options = FirebaseOptions.Builder()
+                .setApiKey("AIzaSyCcNlowv61JgW0Bda9kkdFtLlWA8QZSIIw")
+                .setApplicationId("1:106555594589:android:e743e63b46ed3688ad3ab4")
+                .setProjectId("maarifaapp-aa585")
+                .setStorageBucket("maarifaapp-aa585.firebasestorage.app")
+                .build()
+
+            FirebaseApp.initializeApp(this, options)
+        }
+
+        // Initialize AppContainer baada ya Firebase kuwa tayari
         container = AppContainer.get(this)
     }
 }
