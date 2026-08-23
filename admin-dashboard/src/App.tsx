d941501e274691855ef79d654344a3279ab8c505
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-08-23 21:34:53 +0300

    Update App.tsx

--- admin-dashboard/src/App.tsx
+++ admin-dashboard/src/App.tsx
@@ -1,5 +1,5 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { AdminAuthProvider } from "./lib/AdminAuthContext";
+import { BrowserRouter, Routes, Route, Navigate, Outlet } from "react-router-dom";
+import { AdminAuthProvider, useAdminAuth } from "./lib/AdminAuthContext";
 import { AdminLayout } from "./components/AdminLayout";
 import LoginPage from "./pages/LoginPage";
 import DashboardHome from "./pages/DashboardHome";
@@ -8,19 +8,46 @@ import ContentPage from "./pages/ContentPage";
 import SubscriptionsPage from "./pages/SubscriptionsPage";
 import PayoutsPage from "./pages/PayoutsPage";
 
+// Kizuizi cha kuhakikisha Admin tu ndiye anayeingia (Protected Route)
+function ProtectedRoute() {
+  const { user, loading } = useAdminAuth();
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-emerald-600"></div>
+      </div>
+    );
+  }
+
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+
+  return <Outlet />;
+}
+
 export default function App() {
   return (
     <AdminAuthProvider>
       <BrowserRouter>
         <Routes>
+          {/* Public Route */}
           <Route path="/login" element={<LoginPage />} />
-          <Route element={<AdminLayout />}>
-            <Route path="/" element={<DashboardHome />} />
-            <Route path="/teachers" element={<TeachersPage />} />
-            <Route path="/content" element={<ContentPage />} />
-            <Route path="/subscriptions" element={<SubscriptionsPage />} />
-            <Route path="/payouts" element={<PayoutsPage />} />
+
+          {/* Protected Admin Routes */}
+          <Route element={<ProtectedRoute />}>
+            <Route element={<AdminLayout />}>
+              <Route path="/" element={<DashboardHome />} />
+              <Route path="/teachers" element={<TeachersPage />} />
+              <Route path="/content" element={<ContentPage />} />
+              <Route path="/subscriptions" element={<SubscriptionsPage />} />
+              <Route path="/payouts" element={<PayoutsPage />} />
+            </Route>
           </Route>
+
+          {/* Catch-all 404 Route */}
+          <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </BrowserRouter>
     </AdminAuthProvider>
