c83f0c2d6618e3f59a9376200eaddc874c4de7f3
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-08-28 10:29:19 +0300

    Update NavGraph.kt

--- app/src/main/java/com/maarifa/app/navigation/NavGraph.kt
+++ app/src/main/java/com/maarifa/app/navigation/NavGraph.kt
@@ -1,7 +1,6 @@
 package com.maarifa.app.navigation
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
