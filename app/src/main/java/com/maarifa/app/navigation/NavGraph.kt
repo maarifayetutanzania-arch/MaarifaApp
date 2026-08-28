5fed54ca3fd9bb95f0d607a73582cdbea3a7bbc4
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-08-28 09:40:41 +0300

    Update NavGraph.kt

--- app/src/main/java/com/maarifa/app/navigation/NavGraph.kt
+++ app/src/main/java/com/maarifa/app/navigation/NavGraph.kt
@@ -6,65 +6,30 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 
-// Hakikisha ume-import Screens zako kwa usahihi hapa chini:
-// import com.maarifa.app.ui.screens.LoginScreen
-// import com.maarifa.app.ui.screens.SignUpScreen
-// import com.maarifa.app.ui.screens.HomeScreen
-
-object Routes {
-    const val LOGIN = "login"
-    const val SIGN_UP = "signup"
-    const val HOME = "home"
-}
+// Badilisha au hakikisha imports za screens hapa chini ni sahihi kulingana na mradi wako
+import com.maarifa.app.ui.auth.LoginScreen
+import com.maarifa.app.ui.auth.SignUpScreen
+import com.maarifa.app.ui.auth.SplashScreen
 
 @Composable
-Composable NavGraph(
+fun MaarifaNavGraph(
     navController: NavHostController = rememberNavController(),
-    startDestination: String = Routes.LOGIN
+    startDestination: String = Routes.SPLASH
 ) {
     NavHost(
         navController = navController,
         startDestination = startDestination
     ) {
-        // 1. Login Screen Route
+        composable(Routes.SPLASH) {
+            SplashScreen(navController = navController)
+        }
+
         composable(Routes.LOGIN) {
-            // Badala ya kupitisha 'navController' moja kwa moja,
-            // tunapitisha Lambda callbacks () -> Unit
-            LoginScreen(
-                onNavigateToHome = {
-                    navController.navigate(Routes.HOME) {
-                        popUpTo(Routes.LOGIN) { inclusive = true }
-                    }
-                },
-                onNavigateToSignUp = {
-                    navController.navigate(Routes.SIGN_UP)
-                }
-            )
+            LoginScreen(navController = navController)
         }
 
-        // 2. Sign Up Screen Route
-        composable(Routes.SIGN_UP) {
-            SignUpScreen(
-                onNavigateToHome = {
-                    navController.navigate(Routes.HOME) {
-                        popUpTo(Routes.LOGIN) { inclusive = true }
-                    }
-                },
-                onNavigateToLogin = {
-                    navController.popBackStack()
-                }
-            )
-        }
-
-        // 3. Home Screen Route
-        composable(Routes.HOME) {
-            HomeScreen(
-                onLogout = {
-                    navController.navigate(Routes.LOGIN) {
-                        popUpTo(Routes.HOME) { inclusive = true }
-                    }
-                }
-            )
+        composable(Routes.REGISTER) {
+            SignUpScreen(navController = navController)
         }
     }
 }
