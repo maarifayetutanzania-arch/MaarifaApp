b6ab9f28c9168bcc3fa7f56b3eeeadf023d683ce
maarifayetutanzania-arch <maarifayetutanzania@gmail.com>
2026-09-02 10:39:58 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -79,8 +79,9 @@ android {
 
 dependencies {
     // Core / Compose
-implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.core:core-splashscreen:1.0.1") // Ongezeko kwa ajili ya Splash Screen API
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.4")
     implementation("androidx.activity:activity-compose:1.9.1")
